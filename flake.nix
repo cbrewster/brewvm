a18663230a1b0297e98eef845f522a3e9f2140af
Connor Brewster <cbrewster@hey.com>
2025-10-13 16:42:58 -0500

    Add minimal kernel config

--- flake.nix
+++ flake.nix
@@ -5,8 +5,22 @@
   outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
+
+      base = pkgs.linuxKernel.kernels.linux_6_17;
+
+      # Minimal kernel with virtio-mmio support
+      customKernel = pkgs.linuxKernel.manualConfig {
+        version = base.version;
+        src = base.src;
+        configfile = ./miniconfig;
+      };
     in
     {
+      packages = {
+        kernel = customKernel;
+        default = customKernel;
+      };
+
       devShell = pkgs.mkShell {
         buildInputs = with pkgs; [
           zig
